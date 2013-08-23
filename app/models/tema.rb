e186bde46d8073e3c5bbc21dbd799360a2f2e9f3
Eder A. Orellano <orellano.cni@gmail.com>
2013-08-23 18:53:16 -0500

    Este es mi primer proyecto de fisica

---
+++ app/models/tema.rb
@@ -0,0 +1,3 @@
+class Tema < ActiveRecord::Base
+  attr_accessible :descripcion, :nombre
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Fisica::Application.config.secret_token = 'f6821a480c07fafde30a92eee6048b95cd2de296e9331e53ea3e9cdb21c5729df7dbd315462bd1df4f26817d68f48bc18da78d18481e119ab7fa9d8266db3bde'
